d432f6c2be16e8d69ed9af74e36563b7d8a8df59
Zahir Ekrem <63649974+zahirekrem09@users.noreply.github.com>
2024-04-18 09:34:37 +0200

    add tentamen opdracht

--- src/controller/TafelsLauncher.java
+++ src/controller/TafelsLauncher.java
@@ -6,13 +6,12 @@ import java.util.Scanner;
 
 /**
  * @author Zahir Ekrem SARITEKE
- * @project opdracht_1_2
+ * @project Tafels printen
  * @created 16 April Tuesday 2024 - 21:25
  */
 public class TafelsLauncher {
 
     private static final int AANTAL_GETALLEN_PER_REGEL = 5;
-
 
 
     public static void main(String[] args) {
@@ -26,35 +25,29 @@ public class TafelsLauncher {
         while (true) {
 
             System.out.print("Welke tafel wilt u afdrukken? (0=stoppen) ");
-
-//            int tafelGetal = scanner.nextInt();
             int tafelGetal = CheckScanner.getValidIntInput(scanner);
-
             if (tafelGetal == 0) {
                 System.out.println("Tot ziens!");
                 break;
             }
 
-
             System.out.print("Hoeveel getallen wilt u afdrukken? ");
 
             int aantal = CheckScanner.getValidInput(scanner);
-
             System.out.printf("Tafel van %d : \n", tafelGetal);
 
             // Print the table
             int maxLength = Integer.toString(aantal * tafelGetal).length() + 1;
-
-
+            String formatLength = String.valueOf(maxLength);
             for (int teller = 1; teller <= aantal; teller++) {
                 int product = tafelGetal * teller;
-                //System.out.printf("%d%d ", maxLength, product);
-              System.out.printf("%6d ",  product);
+                System.out.printf("%" + formatLength + "d", product);
+//              System.out.printf("%5d",  maxLength,product);
+
                 if (teller % AANTAL_GETALLEN_PER_REGEL == 0) {
                     System.out.println();
                 }
             }
-
             System.out.println();
         }
     }
---
+++ src/controller/TentamencijfersLauncher.java
@@ -0,0 +1,69 @@
+package controller;
+
+import util.CheckScanner;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+/**
+ * @author Zahir Ekrem SARITEKE
+ * @project  Het analyseren van tentamenresultaten
+ * @created 17 April Wednesday 2024 - 10:18
+ */
+public class TentamencijfersLauncher {
+
+
+    private static final int AANTAL_GETALLEN_PER_REGEL = 5;
+    private static final int MAX_CIJFERS = 10;
+
+    public static void main(String[] args) {
+        printTentamencijfers();
+    }
+
+    private static void printTentamencijfers() {
+
+        System.out.print("Hoeveel cijfers wilt u invoeren?");
+        Scanner scanner = new Scanner(System.in);
+        int aantalCijfers =  CheckScanner.getValidIntInput(scanner);
+
+        // !! check if greater than 0
+        if (aantalCijfers==0) {
+            System.out.println("Aantal cijfers moet groter zijn dan 0!");
+            aantalCijfers =  CheckScanner.getValidIntInput(scanner);
+        }
+
+        double[] cijfers = new double[aantalCijfers];
+        int totaal = 0;
+        int voldoendeAantal = 0;
+        for (int teller = 0; teller < cijfers.length; teller++) {
+            System.out.printf("Cijfer student %d: ", teller + 1);
+
+            cijfers[teller] = CheckScanner.getValidDouble(scanner);
+            if (cijfers[teller] > MAX_CIJFERS) {
+                System.out.println("Cijfer moet langer zijn dan 10!");
+                System.out.printf("Cijfer student %d: ", teller + 1);
+                cijfers[teller] = CheckScanner.getValidDouble(scanner);
+            }
+            if (cijfers[teller] >= AANTAL_GETALLEN_PER_REGEL) {
+                voldoendeAantal++;
+            }
+            totaal += cijfers[teller];
+        }
+
+//        double max = cijfers[0];
+//        for (int teller = 1; teller < cijfers.length; teller++) {
+//            if (cijfers[teller] > max) {
+//                max = cijfers[teller];
+//            }
+//        }
+        double maxCijfer = Arrays.stream(cijfers).max().getAsDouble();
+        double minCijfer = Arrays.stream(cijfers).min().getAsDouble();
+        System.out.printf("Aantal cijfers : %d%n", aantalCijfers);
+        System.out.printf("Gemiddelde cijfer: %.2f%n", (double) totaal / aantalCijfers);
+        System.out.printf("Hoogste cijfer : %.2f%n", maxCijfer);
+        System.out.printf("Laagste cijfer : %.2f%n", minCijfer);
+        System.out.printf("Aantal voldoendes: %d%n", voldoendeAantal );
+
+    }
+}
